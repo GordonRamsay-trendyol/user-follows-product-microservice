c4061098992cdd6ea1d84504ec3ebe691c29429f
Alp <alpars44@hotmail.com>
2021-07-25 21:53:45 +0300

    fix! change mapping name to noun

--- src/main/java/com/gordonramsay/userfollowsproduct/controller/UserFollowsProductController.java
+++ src/main/java/com/gordonramsay/userfollowsproduct/controller/UserFollowsProductController.java
@@ -14,7 +14,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("/products/subscribe")
+@RequestMapping("/products/subscription")
 @RequiredArgsConstructor
 public class UserFollowsProductController {
     private final UserFollowsProductService service;
